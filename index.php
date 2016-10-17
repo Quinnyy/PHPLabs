ee0776cb6172561b6904afc277c813d082ed279d
Quinnyy <michaelstvns2@gmail.com>
2016-10-17 15:13:48 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>index</title>
+</head>
+<body >
+<h1>PHP Lab Work!</h1>
+<h2>This web page will display links to my different lab work</h2>
+</body>
+</html>
+
+
+<?php
+
+
+?>
