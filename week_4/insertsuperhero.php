6f0eab977a789493b8479ecbf8a4a78c1922da0d
Quinnyy <michaelstvns2@gmail.com>
2016-10-17 17:04:15 +0100

    Lab work

--- week_4/insertsuperhero.php
+++ week_4/insertsuperhero.php
@@ -1,7 +1,23 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Insert Superhero to DB</title>
+</head>
+<body >
+
+
+</body>
+</html>
+
+
 <?php
-/**
- * Created by PhpStorm.
- * User: 1505993
- * Date: 17/10/2016
- * Time: 16:53
- */
+
+$con = mysqli_connect("eu-cdbr-azure-west-a.cloudapp.net","ba5b868e496ad8","c2e15cdf","acsm_253596416cc65a9");
+
+if (mysqli_connect_errno()){
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}else{
+    echo "working\r";
+}
+
+?>
