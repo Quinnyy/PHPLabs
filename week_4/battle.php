7f3f6f31bf4c3b4b61123b855845a26cb75c497a
Quinnyy <michaelstvns2@gmail.com>
2016-10-17 18:04:21 +0100

    Lab work

--- week_4/battle.php
+++ week_4/battle.php
@@ -1,7 +1,20 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 1505993
- * Date: 17/10/2016
- * Time: 17:57
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Insert Battle</title>
+</head>
+<body>
+
+<form action="insertsuperhero.php" method="post">
+    <label>Superheros:</label><select></select type="text" name="superhero">
+    <label>Villain Fought:</label><input type="text" name="villain">
+    <input type="submit" value="Submit">
+</form>
+
+
+<a href="manageSuperheros.html"<a>Back!</a>
+
+
+</body>
+</html>
