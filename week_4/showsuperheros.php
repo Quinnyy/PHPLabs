ba316a35a482faeb3c1184f285ba6c59a8945d41
Quinnyy <michaelstvns2@gmail.com>
2016-10-17 17:31:36 +0100

    Lab work

--- week_4/showsuperheros.php
+++ week_4/showsuperheros.php
@@ -1,7 +1,37 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Show Superheros</title>
+</head>
+<body >
+<h2>All Superheros</h2>
+
+<a href="manageSuperheros.html"<a>Back!</a>
+</body>
+</html>
+
 <?php
-/**
- * Created by PhpStorm.
- * User: 1505993
- * Date: 17/10/2016
- * Time: 16:54
- */
+$con = mysqli_connect("eu-cdbr-azure-west-a.cloudapp.net","ba5b868e496ad8","c2e15cdf","acsm_253596416cc65a9");
+
+if (mysqli_connect_errno()){
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}else{
+    echo "working\r";
+}
+
+$sql = "SELECT * FROM superheros ";
+$result = $con->query($sql);
+
+if ($result->num_rows > 0) {
+
+    while($row = $result->fetch_assoc()) {
+        echo "First Name: " . $row["fistName"]. "\r" . "Last Name: " . $row["lastName"]. "/r" . "Super Power: " . $row["mainSuperpower"] . ",";
+        echo "</br>";
+    }
+
+} else {
+    echo "0 results";
+}
+$con->close();
+
+?>
