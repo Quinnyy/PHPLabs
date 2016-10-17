f4680f187e9e6d1d983e522882e5f82b16fbde37
Quinnyy <michaelstvns2@gmail.com>
2016-10-17 16:03:06 +0100

    Lab work

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <h1>PHP Lab Work!</h1>
 <h2>This web page will display links to my different lab work</h2>
 
-<h3>Week 2</h3>
+<h3>Week 3</h3>
 <a href="/week_3/displayDB.php"<a>Display from DB</a>
 <h3>Week 4</h3>
 <a href="/week_4/helloworld.php"<a>Get method</a>
