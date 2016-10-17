f2634f6f1feb7a2d164c1cd391bbc6bbb5a46f8b
Quinnyy <michaelstvns2@gmail.com>
2016-10-17 17:13:11 +0100

    Lab work

--- week_4/insertsuperhero.php
+++ week_4/insertsuperhero.php
@@ -11,13 +11,29 @@
 
 
 <?php
+$forename = $_POST["forename"];
+$surname = $_POST["surname"];
+$power = $_POST["power"];
 
-$con = mysqli_connect("eu-cdbr-azure-west-a.cloudapp.net","ba5b868e496ad8","c2e15cdf","acsm_253596416cc65a9");
+$conn = mysqli_connect("eu-cdbr-azure-west-a.cloudapp.net","ba5b868e496ad8","c2e15cdf","acsm_253596416cc65a9");
 
 if (mysqli_connect_errno()){
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }else{
     echo "working\r";
+
+    $sql = "INSERT INTO superheros (firstName, lastName, mainSuperpower) VALUES ($forename, $surname, $power)";
+    $result = $con->query($sql);
+
+    if ($conn->query($sql) === TRUE) {
+           echo "New record created successfully";
+        }
+ else {
+           echo "Error: " . $sql . "<br>" . $conn->error;
 }
+
+}
+
+$conn->close();
 
 ?>
