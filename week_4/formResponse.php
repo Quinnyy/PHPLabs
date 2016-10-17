effd2c68f7d92cd81a927b685b5059716eba5587
Quinnyy <michaelstvns2@gmail.com>
2016-10-17 16:29:55 +0100

    Lab work

--- week_4/formResponse.php
+++ week_4/formResponse.php
@@ -5,16 +5,18 @@
 </head>
 <body >
 
-<?php
-$forename = $_POST["forename"];
-$surname = $_POST["surname"];
-$power = $_POST["power"];
-echo "Hello {$forename}{$surname}";
 
-//echo "Name: " . $forename . "" . $surname . "Super Power: " . "" . $power;
-
-?>
 
 <a href="set_Superhero.php"<a>Back!</a>
 </body>
 
+<?php
+$forename = $_POST["forename"];
+$surname = $_POST["surname"];
+$power = $_POST["power"];
+
+echo "Name: " . $forename . "" . $surname . "Super Power: " . "" . $power;
+
+?>
+
+</html>
