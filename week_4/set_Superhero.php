719cbd33e55f17c040634a43aeea611674ca6f38
Quinnyy <michaelstvns2@gmail.com>
2016-10-17 16:33:40 +0100

    Lab work

--- week_4/set_Superhero.php
+++ week_4/set_Superhero.php
@@ -8,9 +8,9 @@
 <h2>Create Superhero!</h2>
 
 <form action="formResponse.php" method="post">
-    <label>Forename:</label><input type="text" name="=forename">
-    <label>Surname:</label><input type="text" name="=surname">
-    <label>Super Power:</label><input type="text" name="=power">
+    <label>Forename:</label><input type="text" name="forename">
+    <label>Surname:</label><input type="text" name="surname">
+    <label>Super Power:</label><input type="text" name="power">
     <input type="submit" value="Submit">
 </form>
 
