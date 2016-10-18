2145984058f36e4c9426e95252cf6513f7ae672b
Quinnyy <michaelstvns2@gmail.com>
2016-10-18 17:51:21 +0100

    Lab work

--- week_4/insertbattle.php
+++ week_4/insertbattle.php
@@ -26,7 +26,7 @@ $superhero = $_POST["firstName"];
 $heroID = "SELECT superheroID FROM superheros WHERE firstName = '$superhero'";
 $result = $conn->query($heroID);
 
-echo $result;
+echo '$result';
 $sql = "INSERT INTO battles (superheroID, villanFought) VALUES ($result, '$villain')";
 
 if ($conn->query($sql) === TRUE) {
